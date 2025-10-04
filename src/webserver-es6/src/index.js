8cc9b61072ee10ebf6cbeabba52c33cf5ecc5137
Michael Greenberg <greemi01@gmail.com>
2025-10-04 22:08:29 +0300

    x

--- src/webserver-es6/src/index.js
+++ src/webserver-es6/src/index.js
@@ -1,4 +1,3 @@
-import { match } from 'path-to-regexp';
 /*
 to update certificate
 sudo systemctl stop kjhanna
@@ -16,19 +15,20 @@ import { fileURLToPath } from 'url';
 import fs from 'fs/promises';
 import { constants } from 'fs';
 import { StatusCodes } from 'http-status-codes';
-import { getReasonPhrase } from 'http-status-codes';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-let httpPort = 8080;
-let httpsPort = 443;
+const DEFAULT_HTTP_PORT = 8080;
+const DEFAULT_HTTPS_PORT = 443;
 
-const INDEX_PAGE = '86';
-// const MAIL_SENT = '502';
+const INDEX_PAGE = 'about_kj_hannah_greenberg';
+const ERROR_PAGE = 'page_not_found';
 
-let isProduction;
+const PRODUCTION_KEY_FOLDER = '/etc/letsencrypt/live/kjhannahgreenberg.net';
+const IS_PRODUCTION = await fileExists(PRODUCTION_KEY_FOLDER);
+
 const templateFile = 'template_bootstrap.html';
-let template;
+let page_template;
 
 const PAGE_INFO = new Map();
 
@@ -75,7 +75,7 @@ function dataFile(f) {
 
 
 function securityHeaders(req, res, next) {
-    if (isProduction) {
+    if (IS_PRODUCTION) {
         res.setHeader('Strict-Transport-Security', 'max-age=31536000 ; includeSubDomains');
     }
     // the 'data:' is needed because bootstrap includes some inline images (for the menu bar icon)
@@ -98,43 +98,31 @@ async function fileExists(f) {
 }
 
 
-
-async function returnWebsitePage(res, m1, m2, var1, val1) {
-    try {
-        const pageData = PAGE_INFO.get(m2);
-        if (!pageData) {
-            throw new HttpError(StatusCodes.NOT_FOUND);
-        }
-
-        if (pageData.page_route && m1) {
-            res.redirect(StatusCodes.PERMANENT_REDIRECT, "/" + pageData.page_route);
-            return;
-        }
-        let page = await getWebsitePage(pageData, var1, val1);
-        res.setHeader('content-type', 'text/html; charset=utf8');
-        res.send(page);
-    } catch {
-        throw new HttpError(StatusCodes.NOT_FOUND);
-
-    }
+function replacePlaceholders(text, replacements) {
+    return text.replace(/\{%\w+%\}/g, (match) => {
+        const key = match.slice(2, -2); // strip {% and %}
+        return replacements.has(key) ? replacements.get(key) : match;
+    });
 }
 
-async function getWebsitePage(pageData, var1, val1) {
-    let fn1 = dataFile(`${pageData.page_route}.html`);
-    let fn2 = dataFile(`menu1_${pageData.page_number}.html`);
 
-    const useFile = await fileExists(fn1) ? fn1 : fn2;
-
-    let fileText = await fs.readFile(useFile, 'utf8');
-
-    let page = template.replace('{%content%}', fileText);
-
-    page = page.replaceAll('{%name%}', pageData.page_title);
-
-    if (var1) {
-        page = page.replaceAll(`{%${var1}%}`, val1);
+async function returnWebsitePage(res, pageName) {
+    const pageData = PAGE_INFO.get(pageName);
+    if (!pageData) {
+        throw new HttpError(StatusCodes.NOT_FOUND);
     }
-    return page;
+
+
+    const fileName = dataFile(`${pageData.page_route}.html`);
+    const fileText = await fs.readFile(fileName, 'utf8');
+    const vars = new Map([
+        ['content', fileText],
+        ['name', pageData.page_title]]);
+
+    const page = replacePlaceholders(page_template, vars);
+
+    res.setHeader('content-type', 'text/html; charset=utf8');
+    res.send(page);
 }
 
 
@@ -143,11 +131,11 @@ const app = express();
 app.use(securityHeaders);
 
 app.get('/', async (req, res) => {
-    await returnWebsitePage(res, '1', INDEX_PAGE);
+    await returnWebsitePage(res, INDEX_PAGE);
 });
 
 app.get('/index.html', async (req, res) => {
-    await returnWebsitePage(res, '1', INDEX_PAGE);
+    await returnWebsitePage(res, INDEX_PAGE);
 });
 
 app.get('/page', async (req, res) => {
@@ -156,99 +144,93 @@ app.get('/page', async (req, res) => {
         throw new HttpError(StatusCodes.NOT_FOUND);
     }
 
-    return await returnWebsitePage(res, m[1], m[2]);
+    const [_, m1, m2] = m;
+    const pageData = PAGE_INFO.get(m2);
+
+    if (m1 === '1' && pageData && pageData.page_route) {
+        res.redirect(StatusCodes.PERMANENT_REDIRECT, "/" + pageData.page_route);
+        return;
+    }
+
+    throw new HttpError(StatusCodes.NOT_FOUND);
 });
 
 app.get('/:page', async function (req, res, next) {
-    const page = req.params.page;
-    if (PAGE_INFO.has(page)) {
-        return await returnWebsitePage(res, null, page);
+    const pageName = req.params.page;
+    if (PAGE_INFO.has(pageName)) {
+        return await returnWebsitePage(res, pageName);
     } else {
         return next();
     }
 });
 
-// filter out probes to .well-known
+// filter out probes to .well-known pages w/o any server side error handling
 app.use((req, res, next) => {
     if (req.path.startsWith("/.well-known/") ||
         req.path.startsWith("/favicon.ico")
-) {
+    ) {
         return res.sendStatus(404);
     }
     next();
 });
 
 app.use(express.static(htmlPublic, {
-    setHeaders: (res, filePath) => {
+    setHeaders: (res, _filePath) => {
         res.setHeader('Cache-Control', 'max-age=86400');
     }
 }));
 
-app.use((req, res) => {
+app.use((_req, _res) => {
     // not found should throw error for to get to default error handler
     throw new HttpError(StatusCodes.NOT_FOUND);
 });
 
-app.use(async (err, req, res, next) => {
-    // send all else to page_not_found if possible
+app.use(async (err, req, res, _next) => {
     const code = err.status || 500;
 
     if (code !== StatusCodes.NOT_FOUND) {
-        console.error(err.message);
+        const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+        console.log(`Error while processing ${fullUrl}`);
+        console.error(err);
     }
 
     try {
-        console.log(req.path)
-        return await returnWebsitePage(res, null, "/page_not_found");
+        return await returnWebsitePage(res, ERROR_PAGE);
     } catch (err) {
-        console.log("Redirect to error failed");
+        console.log("Redirect to error page failed");
         console.log(err);
     }
 
-    function safeReasonPhrase(code) {
-        try {
-            return getReasonPhrase(code);
-        } catch {
-            return 'Unknown Status';
-        }
-    }
-
-    res.status(code).send(safeReasonPhrase(code));
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Internal Server Error");
 });
 
 
 async function main() {
-    await setupDb();
+    try {
+        await setupDb();
 
-    let productionKeyFolder = '/etc/letsencrypt/live/kjhannahgreenberg.net';
-    isProduction = await fileExists(productionKeyFolder);
-    let keyFolder = isProduction ? productionKeyFolder : __dirname;
+        page_template = await fs.readFile(webSiteFile(templateFile), 'utf8');
 
-    if (process.argv.length > 2) {
-        httpPort = parseInt(process.argv[2]);
-    }
-
-    if (process.argv.length > 3) {
-        templateFile = process.argv[3];
-    }
-
-    template = await fs.readFile(webSiteFile(templateFile), 'utf8');
-
-    if (isProduction) {
-        try {
-            let privateKey = await fs.readFile(`${keyFolder}/privkey.pem`, 'utf8');
-            let certificate = await fs.readFile(`${keyFolder}/fullchain.pem`, 'utf8');
-            let credentials = { key: privateKey, cert: certificate };
-            let httpsServer = https.createServer(credentials, app);
-            httpsServer.listen(httpsPort);
-            console.log('Started https://localhost:' + httpsPort);
-        } catch (err) {
-            console.log(err);
+        if (IS_PRODUCTION) {
+            try {
+                const httpsPort = (process.argv.length > 2) ?  parseInt(process.argv[2]) : DEFAULT_HTTPS_PORT ;
+                let privateKey = await fs.readFile(`${PRODUCTION_KEY_FOLDER}/privkey.pem`, 'utf8');
+                let certificate = await fs.readFile(`${PRODUCTION_KEY_FOLDER}/fullchain.pem`, 'utf8');
+                let credentials = { key: privateKey, cert: certificate };
+                let httpsServer = https.createServer(credentials, app);
+                httpsServer.listen(httpsPort);
+                console.log('Started https://localhost:' + httpsPort);
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            const httpPort = (process.argv.length > 2) ?  parseInt(process.argv[2]) : DEFAULT_HTTP_PORT ;
+            let httpServer = http.createServer(app);
+            httpServer.listen(httpPort);
+            console.log('Started http://localhost:' + httpPort);
         }
-    } else {
-        let httpServer = http.createServer(app);
-        httpServer.listen(httpPort);
-        console.log('Started http://localhost:' + httpPort);
+    } catch (err) {
+        console.log(err);
     }
 }
 
